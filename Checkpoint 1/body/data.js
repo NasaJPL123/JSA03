89bbcf3d443d43100c4ea68ce7e5a83a2b6ba7c0
NasaJPL123 <Ankhangngoquoc@gmail.com>
2023-05-20 19:49:14 +0700

    a

---
+++ Checkpoint 1/body/data.js
@@ -0,0 +1,56 @@
+export const jsonData = [
+    {
+      "ID": 1,
+      "AdvisorName": "John Smith",
+      "Email": "john.smith@example.com",
+      "Company": "ABC Corp",
+      "Role": "Manager",
+      "FluffleName": "Brassan Fluffle",
+      "Customers": 10
+    },
+    {
+      "ID": 2,
+      "AdvisorName": "Jane Doe",
+      "Email": "jane.doe@example.com",
+      "Company": "XYZ Inc",
+      "Role": "Developer",
+      "FluffleName": "Brassan Fluffle",
+      "Customers": 5
+    },
+    {
+      "ID": 3,
+      "AdvisorName": "Robert Johnson",
+      "Email": "robert.johnson@example.com",
+      "Company": "123 Co",
+      "Role": "Designer",
+      "FluffleName": "Brassan Fluffle",
+      "Customers": 8
+    },
+    {
+      "ID": 4,
+      "AdvisorName": "Sarah Thompson",
+      "Email": "sarah.thompson@example.com",
+      "Company": "XYZ Corp",
+      "Role": "Manager",
+      "FluffleName": "Silver Fluffle",
+      "Customers": 12
+    },
+    {
+      "ID": 5,
+      "AdvisorName": "Michael Brown",
+      "Email": "michael.anderson@example.com",
+      "Company": "456 Co",
+      "Role": "Developer",
+      "FluffleName": "Silver Fluffle",
+      "Customers": 6
+    },
+    {
+      "ID": 6,
+      "AdvisorName": "Emily Brown",
+      "Email": "emily.brown@example.com",
+      "Company": "789 Inc",
+      "Role": "Designer",
+      "FluffleName": "Silver Fluffle",
+      "Customers": 9
+    }
+  ];
