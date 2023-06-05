a68b9ae314fffe0a58fe21aefb746ad4fd2d86b5
NasaJPL123 <Ankhangngoquoc@gmail.com>
2023-06-05 13:04:12 +0700

    a

---
+++ deck-shop 2/CartScreen/cart.js
@@ -0,0 +1,46 @@
+let productsDiv = document.getElementById("products");
+fetch("/deck-shop 2/Data/data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((productList) => {
+    renderItem(productList);
+  });
+
+function renderItem(productList) {
+  const { data } = productList;
+
+  for (let i = 0; i < data.length; i++) {
+    let cart_container = document.createElement("div");
+    cart_container.classList.add("card");
+
+    let img_container = document.createElement("img");
+    img_container.src = data[i].image;
+    img_container.classList.add("img-container");
+
+    cart_container.appendChild(img_container);
+
+    let container = document.createElement("div");
+    container.classList.add("container");
+
+    let h5 = document.createElement("h5");
+    h5.textContent = data[i].name;
+
+    let h6 = document.createElement("h6");
+    h6.innerHTML = `<b>Price: </b>${data[i].price}đ`;
+
+    let button = document.createElement("button");
+    button.textContent = "Thêm vào giỏ hàng";
+    button.onclick = function () {
+      addToCart(data[i].name);
+    };
+
+    container.appendChild(h5);
+    container.appendChild(h6);
+    container.appendChild(button);
+
+    cart_container.appendChild(container);
+
+    productsDiv.appendChild(cart_container);
+  }
+}
--- deck-shop 2/index.js
+++ deck-shop 2/index.js
@@ -1,38 +1,49 @@
-let productsDiv =  document.getElementById("products")
-
-// call data từ file json
-fetch("./Data/data.json")
+let productsDiv = document.getElementById("products");
+fetch("/deck-shop 2/Data/data.json")
   .then((response) => {
-    // convert file json thành promise
     return response.json();
   })
   .then((productList) => {
-    // lấy data từ promise về
-    // gán data đó vào hàm
-    renderItem(productList)
+    renderItem(productList);
   });
 
 function renderItem(productList) {
-  // lấy dữ liệu data là con của product list
-  const {data} = productList;
+  const { data } = productList;
 
-  // vòng lặp để lấy tất cả dữ liệu từ data render ra màn hình
   for (let i = 0; i < data.length; i++) {
-    // tạo cho id proect 1 thẻ div là cart con của products
     let cart_container = document.createElement("div");
-    // thêm class card vào element và ăn theo css
-    cart_container.classList.add("card")
+    cart_container.classList.add("card");
 
     let img_container = document.createElement("img");
     img_container.src = data[i].image;
-    img_container.classList.add("img-container")
+    img_container.classList.add("img-container");
 
-    cart_container.appendChild(img_container)
+    cart_container.appendChild(img_container);
 
-    // thêm giá trị con vào products
-    productsDiv.appendChild(cart_container)
+    let container = document.createElement("div");
+    container.classList.add("container");
+
+    let h5 = document.createElement("h5");
+    h5.textContent = data[i].name;
+
+    let h6 = document.createElement("h6");
+    h6.innerHTML = `<b>Price: </b>${data[i].price}đ`;
+
+    let button = document.createElement("button");
+    button.textContent = "Thêm vào giỏ hàng";
+    button.onclick = function () {
+      addToCart(data[i].name);
+    };
+
+    container.appendChild(h5);
+    container.appendChild(h6);
+    container.appendChild(button);
+
+    cart_container.appendChild(container);
+
+    productsDiv.appendChild(cart_container);
   }
-} 
+}
 
 
 // bài tập: render name, price, button thêm giỏ hàng
